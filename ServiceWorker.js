5f9ed4e48b2934ef7e8fc3d23bd380bc9136a937
Saiful Islam <saifulanik@icloud.com>
2026-04-13 10:52:44 +0600

    endpoint fix

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Rofuclav-Rofuclav Beats Bacteria-2.2";
+const cacheName = "Rofuclav-Rofuclav Beats Bacteria-2.4";
 const contentToCache = [
     "Build/Slingshot.loader.js",
     "Build/Slingshot.framework.js",
