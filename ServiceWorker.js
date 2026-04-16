08b42c97e9a369d20001e0d91f87954e9d769ecf
Saiful Islam <saifulanik@icloud.com>
2026-04-16 21:24:00 +0600

    stuff

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Rofixim-Rofixim Beats Bacteria-2.5";
+const cacheName = "Rofixim-Rofixim Beats Bacteria-2.7";
 const contentToCache = [
     "Build/Slingshot.loader.js",
     "Build/Slingshot.framework.js",
