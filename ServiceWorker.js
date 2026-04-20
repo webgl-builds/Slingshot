3f7a69f39305195c2166d83d1aef0ac77c2c05a9
Saiful Islam <saifulanik@icloud.com>
2026-04-21 00:03:09 +0600

    dr id remove

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Rofixim-Rofixim Beats Bacteria-2.7";
+const cacheName = "Rofixim-Rofixim Beats Bacteria-2.8";
 const contentToCache = [
     "Build/Slingshot.loader.js",
     "Build/Slingshot.framework.js",
