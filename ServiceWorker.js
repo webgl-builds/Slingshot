684eb746a9e6d72609068612245957d4de889956
Saiful Islam <saifulanik@icloud.com>
2026-07-05 20:46:06 +0600

    leaderboard build

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Rofuclav-Rofuclav Beats Bacteria-2.8";
+const cacheName = "Rofuclav-Rofuclav Beats Bacteria-3.0";
 const contentToCache = [
     "Build/Slingshot.loader.js",
     "Build/Slingshot.framework.js",
